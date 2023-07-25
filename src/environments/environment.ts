87dfafb574fa9b5c11624e0047a9ef805019119d
Juan Rodriguez <phjuanrodriguez@gmail.com>
2023-07-25 20:16:56 -0300

    Se agregan iconos en svg.. se pueden hacer dinamicos?

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,16 +4,16 @@
 
 export const environment = {
   production: false,
-  // baseUrl: 'http://localhost:4200/#/',
-  baseUrl: 'http://devcashflow.icornio.com/#/',
+  baseUrl: 'http://localhost:4200/#/',
+  // baseUrl: 'http://devcashflow.icornio.com/#/',
 
   // local
-  // supabaseUrl: 'http://localhost:54321',
-  // supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
+  supabaseUrl: 'http://localhost:54321',
+  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0',
   
   // Dev remoto
-  supabaseUrl: 'https://mdksvmcmnozfbyvbgpss.supabase.co',
-  supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ka3N2bWNtbm96ZmJ5dmJncHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk2MTkxODYsImV4cCI6MjAwNTE5NTE4Nn0.kTbZLzpW5n8Z53g-8UOZF3Qx_WeZcc_-wxD7ZcF4ZAA',
+  // supabaseUrl: 'https://mdksvmcmnozfbyvbgpss.supabase.co',
+  // supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1ka3N2bWNtbm96ZmJ5dmJncHNzIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODk2MTkxODYsImV4cCI6MjAwNTE5NTE4Nn0.kTbZLzpW5n8Z53g-8UOZF3Qx_WeZcc_-wxD7ZcF4ZAA',
 
 }
 
